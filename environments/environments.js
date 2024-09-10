6afbaceabbfdb222f2132c85e3ebe7eed76de31d
JoseLorenzana272 <jodanielorenzana@gmail.com>
2024-09-09 19:09:24 -0600

    feat: Minor Corrections

--- environments/environments.js
+++ environments/environments.js
@@ -1,13 +1,16 @@
 import { Literal } from "../JS_Analyzer_parts/nodos.js";
+import { embedded } from "../Instructions/embedded.js";
 
 export class Entorno {
-
     /**
      * @param {Entorno} padre
      */
     constructor(padre = undefined) {
         this.valores = {};
         this.padre = padre;
+        Object.entries(embedded).forEach(([nombre, funcion]) => {
+            this.setOrUpdateVariable("string", nombre, funcion);
+        });
     }
 
     /**
@@ -16,10 +19,6 @@ export class Entorno {
      * @param {any} valor
      */
     setVariable(tipo, nombre, valor) {
-        /*if (this.valores[nombre]) {
-            throw new Error(`Variable ${nombre} ya definida`);
-        }*/
-        
         if (Array.isArray(valor)) {
             // Si es un array, se crea un nuevo Literal almacenando una copia del array
             this.valores[nombre] = new Literal({ value: [...valor], type: tipo });
@@ -27,12 +26,11 @@ export class Entorno {
             // Para otros tipos de datos, se almacena directamente
             this.valores[nombre] = new Literal({ value: valor, type: tipo });
         }
-
     }
 
     /**
      * @param {string} nombre
-     * @returns {{ tipo: string, valor: any }}
+     * @returns {Literal}
      */
     getVariable(nombre) {
         const valorActual = this.valores[nombre];
@@ -63,5 +61,24 @@ export class Entorno {
         }
 
         throw new Error(`Variable ${nombre} no definida`);
+    }
+
+    /**
+     * @param {string} tipo
+     * @param {string} nombre
+     * @param {any} valor
+     */
+    setOrUpdateVariable(tipo, nombre, valor) {
+        if (this.valores.hasOwnProperty(nombre)) {
+            console.log(`Actualizando variable ${nombre} con valor: ${valor}`);
+            if (this.valores[nombre] instanceof Literal) {
+                this.valores[nombre].value = valor;
+            } else {
+                this.valores[nombre] = new Literal({ value: valor, type: tipo });
+            }
+        } else {
+            console.log(`Guardando nueva variable ${nombre} con valor: ${valor}`);
+            this.setVariable(tipo, nombre, valor);
+        }
     }
 }
